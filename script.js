ffcf589eac3abdb49ff7101db0963589a4853a12
Prashant Salve <128687203+prashant04071995@users.noreply.github.com>
2024-04-24 14:07:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 function getFormvalue() {
-  // Preventing the form from actually submitting (useful if this code is tested in a real environment where the form causes page reload)
-  event.preventDefault();
+  event.preventDefault(); // Prevent the form from submitting traditionally
 
-  // Accessing the form values
-  var firstName = document.forms["form1"]["first_name"].value;
-  var lastName = document.forms["form1"]["last_name"].value;
+  var firstName = document.forms["form1"]["fname"].value; // Updated to 'fname'
+  var lastName = document.forms["form1"]["lname"].value; // Updated to 'lname'
 
-  // Displaying the collected values
   alert(firstName + " " + lastName);
 }
